e8a13ca3d829104ef2b77c5ef742583686881f07
George Olson <georgeo@slalom.com>
2017-05-11 21:51:46 -0700

    Updated to 1.8.6

---
+++ src/Slalom.Stacks.EntityFramework/EndPoints/GetConfiguration.cs
@@ -0,0 +1,38 @@
+﻿/* 
+ * Copyright (c) Stacks Contributors
+ * 
+ * This file is subject to the terms and conditions defined in
+ * the LICENSE file, which is part of this source code package.
+ */
+
+using Microsoft.Extensions.Configuration;
+using Slalom.Stacks.Services;
+
+namespace Slalom.Stacks.EntityFramework.EndPoints
+{
+    /// <summary>
+    /// Gets the Entity Framework configuration.
+    /// </summary>
+    [EndPoint("_system/configuration/entity-framework")]
+    public class GetConfiguration : EndPoint
+    {
+        private readonly IConfiguration _configuration;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetConfiguration" /> class.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        public GetConfiguration(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <inheritdoc />
+        public override void Receive()
+        {
+            var options = new EntityFrameworkOptions();
+            _configuration.GetSection("Stacks:EntityFramework").Bind(options);
+            this.Respond(options);
+        }
+    }
+}
