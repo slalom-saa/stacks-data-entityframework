6667abc96ce026a168822543e327fe09ff88ea9d
George Olson <georgeo@slalom.com>
2017-05-19 06:08:58 -0700

    Added migrations for search and configuration.

--- src/Slalom.Stacks.EntityFramework/EntityFrameworkOptions.cs
+++ src/Slalom.Stacks.EntityFramework/EntityFrameworkOptions.cs
@@ -22,6 +22,12 @@ namespace Slalom.Stacks.EntityFramework
         public string ConnectionString { get; set; } = "Data Source=.;Initial Catalog=Stacks.Search;Integrated Security=True;MultipleActiveResultSets=True";
 
         /// <summary>
+        /// Gets or sets a value that indicates whether or not EF code first migrations should apply.
+        /// </summary>
+        /// <value>A value that indicates whether or not EF code first migrations should apply.</value>
+        public bool EnableMigrations { get; set; } = false;
+
+        /// <summary>
         /// Sets the connection string to use.
         /// </summary>
         /// <param name="connectionString">The connection string.</param>
